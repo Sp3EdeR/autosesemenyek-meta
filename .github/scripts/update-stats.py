ac8ff6feb8f2dc060cfc472d406e6ca2b42138b6
Speeder <ek.speeder@gmail.com>
2025-05-13 22:59:06 +0200

    Updated statistics event filter

--- .github/scripts/update-stats.py
+++ .github/scripts/update-stats.py
@@ -15,8 +15,9 @@ def main():
     print("Parsing ICAL data...")
     calendar = icalendar.Calendar.from_ical(icsData)
 
-    print("Parsing first event's description as JSON...")
-    jsonData = json.loads(calendar.events[0].get("DESCRIPTION"))
+    print("Parsing the statistics event's description as JSON...")
+    statsEvent = [x for x in calendar.events if x.get("SUMMARY") == "statistics"][0]
+    jsonData = json.loads(statsEvent.get("DESCRIPTION"))
 
     print("Writing JSON data to %s..." % args.json_path)
     with open(args.json_path, 'w') as f:
